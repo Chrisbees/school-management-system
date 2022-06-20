4727af33f388c2865486c5cb61c9b562bd6b86d6
Chrisbees <beesworld1@yahoo.com>
2022-06-21 00:52:35 +0100

    mj

--- src/main/java/com/chrisbees/management/security/SecurityConfig.java
+++ src/main/java/com/chrisbees/management/security/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(
                         "/",
                         "/favicon.ico",
+                        "/**/*",
                         "/**/*.png",
                         "/**/*.gif",
                         "/**/*.svg",
