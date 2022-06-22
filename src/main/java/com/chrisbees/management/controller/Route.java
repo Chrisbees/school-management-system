2b4d4daeacfc80929136bdcf76898d30458d83a6
Chrisbees <beesworld1@yahoo.com>
2022-06-23 00:46:18 +0100

    mm

--- src/main/java/com/chrisbees/management/controller/Route.java
+++ src/main/java/com/chrisbees/management/controller/Route.java
@@ -3,12 +3,15 @@ package com.chrisbees.management.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Controller
 public class Route {
 
         @RequestMapping(value = "/**{path:[^\\\\.]*}")
-        public String redirect() {
+        public void redirect(HttpServletResponse response) throws IOException {
             // Forward to home page so that angular routing is preserved.
-            return "forward:/";
+            response.sendRedirect("/");
         }
     }
