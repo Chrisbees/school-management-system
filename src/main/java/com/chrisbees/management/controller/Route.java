08c42e0d8bbb1db58e87e070db7a20eddcd30fab
Chrisbees <beesworld1@yahoo.com>
2022-06-23 00:35:29 +0100

    new

---
+++ src/main/java/com/chrisbees/management/controller/Route.java
@@ -0,0 +1,14 @@
+package com.chrisbees.management.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Route {
+
+        @RequestMapping(value = "/**{path:[^\\\\.]*}")
+        public String redirect() {
+            // Forward to home page so that angular routing is preserved.
+            return "forward:/";
+        }
+    }
