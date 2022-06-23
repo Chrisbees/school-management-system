0944fc8458307c7e1cf97e5cc6cadb65f93716e7
Chrisbees <beesworld1@yahoo.com>
2022-06-23 23:27:01 +0100

    mm

--- src/main/java/com/chrisbees/management/controller/Route.java
+++ src/main/java/com/chrisbees/management/controller/Route.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class Route {
  //route all non-api requests to index.html with regex matching
-    @RequestMapping(value = "/{[^\\.]*}")
+    @RequestMapping(value = "/")
     public String index() {
         return "forward:/index.html";
     }
