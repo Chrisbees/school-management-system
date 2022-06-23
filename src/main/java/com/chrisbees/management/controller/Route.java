b0e8beda15b154e1fd30352e6abd177237babac3
Chrisbees <beesworld1@yahoo.com>
2022-06-23 23:35:24 +0100

    m

--- src/main/java/com/chrisbees/management/controller/Route.java
+++ src/main/java/com/chrisbees/management/controller/Route.java
@@ -1,17 +1,21 @@
 package com.chrisbees.management.controller;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.http.HttpServletResponse;
 
-@Controller
-public class Route {
- //route all non-api requests to index.html with regex matching
-    @RequestMapping(value = "/")
-    public String index() {
-        return "forward:/index.html";
+@Configuration
+public class Route implements WebMvcConfigurer {
+ //route all non-api requests to index.html
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
     }
+
     
 
 }
