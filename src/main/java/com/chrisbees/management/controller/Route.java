973a7db2cb1a121ad76dc9a50dc05314db8bad21
Chrisbees <beesworld1@yahoo.com>
2022-06-23 23:11:14 +0100

    latest commit

--- src/main/java/com/chrisbees/management/controller/Route.java
+++ src/main/java/com/chrisbees/management/controller/Route.java
@@ -3,12 +3,15 @@ package com.chrisbees.management.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class Route {
- //non-restful routes here (e.g. login, register, etc.)
-    @RequestMapping("/")
+ //route all non-api requests to index.html with regex matching
+    @RequestMapping(value = "/{[^\\.]*}")
     public String index() {
         return "forward:/index.html";
     }
+    
 
 }
