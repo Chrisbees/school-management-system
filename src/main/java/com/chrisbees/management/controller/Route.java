5c49cdf173e5e3976e0ecac21b36f5a4443e2abc
Chrisbees <beesworld1@yahoo.com>
2022-06-23 21:59:23 +0100

    m

--- src/main/java/com/chrisbees/management/controller/Route.java
+++ src/main/java/com/chrisbees/management/controller/Route.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Route {
 
     //regex to match url pattern for all requests except /api requests (i.e. /, /login, /register)
-    @RequestMapping(value = "/regex:[^/api]/**")
+    @RequestMapping(value = "{_:^((?!api).)*$}")
     public String index() {
-        return "forward:/index.html";
+        return "forward:/";
     }
 
     //route requests like /login, /register. /, to index.html
